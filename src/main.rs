58e23ee314af495d189cb600a8dcd56cfa9679a8
Riphiphip <halvor.bjorstad@gmail.com>
2022-09-26 17:03:54 +0200

    Set up debugging

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 #![no_std]
 #![no_main]
 
-use bsp::entry;
+use bsp::{entry, hal::uart::Pins};
 use defmt::*;
 use defmt_rtt as _;
 use embedded_hal::digital::v2::OutputPin;
@@ -17,7 +17,7 @@ use rp_pico as bsp;
 
 use bsp::hal::{
     clocks::{init_clocks_and_plls, Clock},
-    pac,
+    pac, pwm,
     sio::Sio,
     watchdog::Watchdog,
 };
@@ -44,8 +44,6 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-
     let pins = bsp::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
@@ -53,14 +51,30 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    let mut led_pin = pins.led.into_push_pull_output();
+    let pwm_slices = pwm::Slices::new(pac.PWM, &mut pac.RESETS);
+
+    let mut pwm = pwm_slices.pwm0;
+    pwm.set_div_int(19);
+    pwm.set_ph_correct();
+    pwm.enable();
+
+    let mut led_pwm_channel = pwm.channel_a;
+    let _led_pwm_channel_pin = led_pwm_channel.output_to(pins.gpio0);
+
+    led_pwm_channel.enable();
+
+    use embedded_hal::PwmPin;
+    led_pwm_channel.set_duty(0xffff / 2);
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
     loop {
         info!("on!");
-        led_pin.set_high().unwrap();
+
+        led_pwm_channel.set_duty(0xffff / 2);
         delay.delay_ms(500);
         info!("off!");
-        led_pin.set_low().unwrap();
+
+        led_pwm_channel.set_duty(0xffff);
         delay.delay_ms(500);
     }
 }
